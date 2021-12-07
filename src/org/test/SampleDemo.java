c915dac132403cd9f28264dd8183b58cd1f96d51
preethies36 <preethies36@gmail.com>
2021-12-07 01:07:15 -0800

    Subtraction

--- src/org/test/SampleDemo.java
+++ src/org/test/SampleDemo.java
@@ -1,5 +1,12 @@
 package org.test;
 
 public class SampleDemo {
-
+	public static void subtraction() {
+		int a =20;
+		int b=10;
+		System.out.println("Subtraction:"+(a-b));
+}
+public static void main(String[] args) {
+	subtraction();
+}
 }
